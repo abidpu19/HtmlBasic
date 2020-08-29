45fe112fffa1a9e1cd433351ca844f4ef3fa3dce
abidpu19 <4838297+abidpu19@users.noreply.github.com>
2020-08-29 11:30:59 +0200

    Style sheet updated
    Signup form style updated

---
+++ Proj_one/script.js
@@ -0,0 +1,10 @@
+var preloader = document.getElementById("loading");
+
+window.onload = function () {
+  setTimeout(hidefunc, 2000);
+};
+
+function hidefunc() {
+  DelayNode;
+  preloader.style.display = "none";
+}
